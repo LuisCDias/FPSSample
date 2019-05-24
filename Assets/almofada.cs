63c2ce41d24ccba93f115e032412cb43a895ab4f
Luís Dias <lcdias127@gmail.com>
2019-05-24 16:13:13 +0100

    v 0.1

---
+++ Assets/almofada.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class almofada : MonoBehaviour
+{
+     // This Will Configure the  AudioSource Component; 
+     // MAke Sure You added AudioSouce to death Zone;
+     void Start ()   
+     {
+        
+     }        
+ 
+     private void OnTriggerEnter(Collider tetast)
+     {
+         GetComponent<AudioSource> ().Play ();
+     }
+}
